e1457973defae0e30778b6ae2504d64f0f54d015
Cas-Dec <casdecancq@hotmail.be>
2024-11-12 14:39:48 +0100

    Test twee

--- scripts/config.py
+++ scripts/config.py
@@ -10,6 +10,7 @@ from pathlib import Path
 ABS_PATH = Path(__file__).resolve().parent.parent
 #DATA_DIR = ABS_PATH / 'data' 
 DATA_DIR = "/data/gent/vo/000/gvo00090/HEAT/Data"
+# jahaha
 #############################################################
 
 ORAS5_LIST = ['depth_of_14_c_isotherm', 'depth_of_17_c_isotherm', 'depth_of_20_c_isotherm',
